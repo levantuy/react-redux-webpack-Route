93fe553714412a50ece17570793edf17d25c756a
levantuy <levantuy.it@gmail.com>
2018-07-20 02:04:16 +0700

    aaaa

--- react-redux-example/src/components/app/App.js
+++ react-redux-example/src/components/app/App.js
@@ -1,11 +1,46 @@
 import React, {Component, PropTypes} from 'react';
+import { FormControl, Button, FormGroup, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import logo from '../../logo.svg';
 
 export default class App extends Component {    
 
     render() {
         return (
             <div>
-                {this.props.children}
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <img src={logo} className="App-logo" alt="Noticer" href="/" />
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <NavItem href="/">Home</NavItem>
+                            <NavItem href="/about-us">About</NavItem>
+                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                                <MenuItem eventKey={3.1}>Action</MenuItem>
+                                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                                <MenuItem divider />
+                                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                            </NavDropdown>
+                        </Nav>
+                        <Navbar.Form pullLeft>
+                            <FormGroup>
+                                <FormControl type="text" placeholder="Search" onChange={this.handleOnChange} />
+                            </FormGroup>{' '}
+                            <Button type="submit" onClick={this.btnSearchClick}>Submit</Button>
+                        </Navbar.Form>
+                        <Nav pullRight>
+                            <NavItem eventKey={1} href="#">Link Right</NavItem>
+                            <NavItem eventKey={2} href="#">Link Right</NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <div className="container">
+                    {this.props.children}
+                </div>                
             </div>
         );
     }
